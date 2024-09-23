cb58237433d4b4837422758980469ec7e86078c6
deba-maity <debajyoti.maity29@gmail.com>
2024-09-23 16:02:21 +0530

    untracked

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProductlistComponent } from './components/productlist/productlist.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, // Default route
+  { path: 'products', component: ProductlistComponent }, // Route for product list
+];
